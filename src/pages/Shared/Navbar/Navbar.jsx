edddfeef2dcc22e1e0f4573195b1d6d48202b50a
aothymoon59 <mmoon8745@gmail.com>
2023-06-08 17:32:07 +0600

    navbar added

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -1,8 +1,65 @@
+import { Link, NavLink } from "react-router-dom";
 import Container from "../Container/Container";
 
 const Navbar = () => {
+  const navOptions = (
+    <>
+      <li>
+        <NavLink
+          to="/"
+          title="Home"
+          className={({ isActive }) =>
+            `px-3 transition-colors duration-300 transform  hover:bg-gray-300  hover:text-gray-700 ${
+              isActive ? " text-[#13795B]" : "text-black"
+            }`
+          }
+        >
+          <span className=" lg:text-lg font-medium">Home</span>
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/instructors"
+          title="Instructors"
+          className={({ isActive }) =>
+            `px-3 transition-colors duration-300 transform  hover:bg-gray-300  hover:text-gray-700 ${
+              isActive ? " text-[#13795B]" : "text-black"
+            }`
+          }
+        >
+          <span className=" lg:text-lg font-medium">Instructors</span>
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/classes"
+          title="Classes"
+          className={({ isActive }) =>
+            `px-3 transition-colors duration-300 transform  hover:bg-gray-300 hover:text-gray-700 ${
+              isActive ? " text-[#13795B]" : "text-black"
+            }`
+          }
+        >
+          <span className=" lg:text-lg font-medium">Classes</span>
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/dashboard"
+          title="Dashboard"
+          className={({ isActive }) =>
+            `px-3 transition-colors duration-300 transform  hover:bg-gray-300  hover:text-gray-700 ${
+              isActive ? " text-[#13795B]" : "text-black"
+            }`
+          }
+        >
+          <span className=" lg:text-lg font-medium">Dashboard</span>
+        </NavLink>
+      </li>
+    </>
+  );
   return (
-    <div className="fixed w-full opacity-25 bg-[#20C997] text-black z-10 shadow-sm">
+    <div className="fixed w-full my-bg text-black z-10 shadow-sm">
       <Container>
         <div className="navbar ">
           <div className="navbar-start">
@@ -25,54 +82,39 @@ const Navbar = () => {
               </label>
               <ul
                 tabIndex={0}
-                className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+                className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 uppercase"
               >
-                <li>
-                  <a>Item 1</a>
-                </li>
-                <li>
-                  <a>Parent</a>
-                  <ul className="p-2">
-                    <li>
-                      <a>Submenu 1</a>
-                    </li>
-                    <li>
-                      <a>Submenu 2</a>
-                    </li>
-                  </ul>
-                </li>
-                <li>
-                  <a>Item 3</a>
-                </li>
+                {navOptions}
               </ul>
             </div>
-            <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+            <Link to="/">
+              <div className="flex justify-center items-center">
+                <span className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-medium sm:font-bold">
+                  Soul
+                </span>
+                <img
+                  src={"https://i.ibb.co/fCxzStJ/darklogo.png"}
+                  className="w-6 sm:w-8 md:w-10 lg:w-12"
+                  alt="Nav Logo"
+                />
+                <span className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-medium sm:font-bold">
+                  Bliss
+                </span>
+              </div>
+            </Link>
           </div>
           <div className="navbar-center hidden lg:flex">
-            <ul className="menu menu-horizontal px-1">
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li tabIndex={0}>
-                <details>
-                  <summary>Parent</summary>
-                  <ul className="p-2">
-                    <li>
-                      <a>Submenu 1</a>
-                    </li>
-                    <li>
-                      <a>Submenu 2</a>
-                    </li>
-                  </ul>
-                </details>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
+            <ul className="menu menu-horizontal px-1 uppercase">
+              {navOptions}
             </ul>
           </div>
           <div className="navbar-end">
-            <a className="btn">Button</a>
+            <Link
+              to="/login"
+              className="my-btn hover:bg-transparent hover:text-[#13795B] transition-all duration-200 ease-linear"
+            >
+              Login
+            </Link>
           </div>
         </div>
       </Container>
@@ -81,3 +123,5 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+// https://i.ibb.co/jDpVk8Q/lightlogo.png
