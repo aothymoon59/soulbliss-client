65dc3b11e622cbd188f8b53d3c37ef766190d6cb
aothymoon59 <mmoon8745@gmail.com>
2023-06-08 18:24:25 +0600

    private firebase api key by env

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCy6R6Y9P3uqA77yvHaLTshkhxrkHWIibU",
-  authDomain: "soulbliss-77696.firebaseapp.com",
-  projectId: "soulbliss-77696",
-  storageBucket: "soulbliss-77696.appspot.com",
-  messagingSenderId: "622759500333",
-  appId: "1:622759500333:web:fa07c6f44276ec9ed2b6a4",
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
