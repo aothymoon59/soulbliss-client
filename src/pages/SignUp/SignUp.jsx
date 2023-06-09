b77bc9684b25b288a6999702d0643b709b5c8bc4
aothymoon59 <mmoon8745@gmail.com>
2023-06-09 12:15:35 +0600

    img upload functionality added

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -20,12 +20,33 @@ const SignUp = () => {
     const password = data.password;
     const confirm = data.confirm;
 
+    // TODO: use toast here
     if (password !== confirm) {
       alert("Confirm password no match");
       return;
     }
-    console.log(data);
+
+    // upload image
+    const image = data.image[0];
+    const formData = new FormData();
+    formData.append("image", image);
+    const url = `https://api.imgbb.com/1/upload?key=${
+      import.meta.env.VITE_IMGBB_KEY
+    }`;
+
+    fetch(url, {
+      method: "POST",
+      body: formData,
+    })
+      .then((res) => res.json())
+      .then((imgData) => {
+        console.log(imgData.data.display_url);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
   };
+
   return (
     <Container>
       <div className="flex flex-col lg:flex-row-reverse justify-center items-center gap-10 my-20">
@@ -69,6 +90,20 @@ const SignUp = () => {
               />
               {errors.email && (
                 <span className="text-red-600 mt-2">Email is required</span>
+              )}
+            </div>
+            <div className="form-control mb-4">
+              <label className="label">
+                <span className="label-text">Upload your Photo</span>
+              </label>
+              <input
+                type="file"
+                {...register("image", { required: true })}
+                name="image"
+                className="file-input file-input-bordered w-full"
+              />
+              {errors.image && (
+                <span className="text-red-600 mt-2">Photo is required</span>
               )}
             </div>
             <div className="form-control mb-6 relative">
