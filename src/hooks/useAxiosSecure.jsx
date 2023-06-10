cb4a5a131b76644a65ca6042630ba7740d141b97
aothymoon59 <mmoon8745@gmail.com>
2023-06-10 22:46:24 +0600

    create axiosSecure hook

---
+++ src/hooks/useAxiosSecure.jsx
@@ -0,0 +1,41 @@
+import { useEffect } from "react";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const useAxiosSecure = () => {
+  const { logOut } = useAuth();
+  const navigate = useNavigate();
+
+  const axiosSecure = axios.create({
+    baseURL: `${import.meta.env.VITE_API_URL}`,
+  });
+
+  useEffect(() => {
+    axiosSecure.interceptors.request.use((config) => {
+      const token = localStorage.getItem("access-token");
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+      return config;
+    });
+
+    axiosSecure.interceptors.response.use(
+      (response) => response,
+      async (error) => {
+        if (
+          (error.response && error.response.status === 401) ||
+          error.response.status === 403
+        ) {
+          await logOut();
+          navigate("/login");
+        }
+        return Promise.reject(error);
+      }
+    );
+  }, [logOut, navigate, axiosSecure]);
+
+  return [axiosSecure];
+};
+
+export default useAxiosSecure;
