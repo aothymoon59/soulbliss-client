6cf95c0ac526f88345e7f58fc30ad23563a044c0
aothymoon59 <mmoon8745@gmail.com>
2023-10-09 02:57:13 +0600

    update discount time

--- src/pages/Home/Discount/Discount.jsx
+++ src/pages/Home/Discount/Discount.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 const Discount = () => {
   const calculateTimeLeft = () => {
-    const discountEndDate = new Date("2023-08-31"); // Set the end date of the discount
+    const discountEndDate = new Date("2023-10-30"); // Set the end date of the discount
     const currentDate = new Date();
     const difference = discountEndDate - currentDate;
 
